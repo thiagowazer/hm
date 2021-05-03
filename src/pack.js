4e775f2434588c6654a7f535a10757473beb823b
hidrabotz <hidrabotz@gmail.com>
2021-05-03 16:43:36 -0400

    First commit

---
+++ src/pack.js
@@ -0,0 +1,44 @@
+const pack = (date, user, wame) => {
+return `┏━🔥࿗ 𝙷𝚒𝚍𝚛𝚊-𝚋𝚘𝚝 𝚞𝚕𝚝𝚒𝚖𝚊𝚝𝚎 ࿗🔥━┓
+║                                                           
+║ _*🕐 𝐃𝐚𝐭𝐚 𝐞 𝐡𝐨𝐫𝐚: ${date} 🕐*_
+║ _*🙂 𝐔𝐬𝐮𝐚́𝐫𝐢𝐨: ${user} 🙂*_
+║ _*🌎 𝐖𝐚𝐦𝐞: ${wame} 🌎*_
+║ _*🌹 𝐃𝐨𝐧𝐨: wa.me/+5567996005372 🌹*_  
+║ *🌹 𝐃𝐨𝐧𝐨: ᬊ͜͡𝐇𝐈𝐃𝐑𝐀ꪶ♾ꫂ𝐌𝐔𝐆𝐄𝐍 🌹*                                      
+║                                                           
+┣══════𝑺𝒊𝒈𝒂 𝒏𝒂𝒔 𝒓𝒆𝒅𝒆𝒔 𝒔𝒐𝒄𝒊𝒂𝒊𝒔══════┫
+║
+║ _*📷 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦:*_ hidrabotz
+║
+║ _*🤖𝐆𝐢𝐭𝐡𝐮𝐛:*_
+║ _*🌐....oculta....🌐*_
+║
+║ _*📹 𝐘𝐨𝐮𝐭𝐮𝐛𝐞:*_
+║ _*🌐..tenho canal n..🌐*_
+║
+┗════════════════════════┛
+» 🥵𝙾𝚗𝚕𝚢𝚏𝚊𝚗𝚜 𝚎 𝚑𝚎𝚗𝚝𝚊𝚒 𝚗𝚘 𝚖𝚎𝚐𝚊🥵 «
+
+❁❧ *FOTOS DE HENTAI 720MB:* 
+_*🌐 https://suaurl.com/55eb2f 🌐*_
+
+❁❧ *PRINCESS STARLING(ONLY FANS)8.36GB:* 
+_*🌐 https://suaurl.com/b995e2 🌐*_
+
+❁❧ *ELLES CLUB 28.68GB:* 
+_*🌐 https://suaurl.com/8c135b 🌐*_
+
+❁❧ *COCONUTKITTY(ONLY FANS)7.69GB:*
+_*🌐 https://suaurl.com/5e91e0 🌐*_
+
+❁❧ *Belle delphine:* 
+_*🌐 https://suaurl.com/5c699d 🌐*_
+
+❁❧ *Love Lilah:* 
+_*🌐 https://suaurl.com/dbaae7 🌐*_
+
+❁❧ *Hidori:* 
+_*🌐 https://suaurl.com/f43f76 🌐*_`
+}
+exports.pack = pack
